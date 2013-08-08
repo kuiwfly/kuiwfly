791e57540131b3b41dd03ed58a67add27c69d4b9
kui.wfly <kui.wfly@gmail.com>
2013-08-08 22:38:08 +0800

    test

--- main.cpp
+++ main.cpp
@@ -6,6 +6,16 @@ int main()
 {
     struct frame_list_t *list = (struct frame_list_t*)malloc(sizeof(struct frame_list_t)) ;
     init_frame_list(list,100) ;
+    struct frame_t *frame = NULL;
+    for(int i=0;i<50;i++){
+        frame = new_frame(i,640,480) ;
+        push_frame_list(list,frame) ;
+    }
+    for(int i=0 ; i<50 ;i++){
+        frame = pop_frame_list(list) ;
+        cout<<"frame id:"<<frame->i_frame<<endl ;
+    }
+    delete_frame_list(list) ;
     cout<<list->i_max_size<<endl ;
     cout<<"hello world"<<endl ;
     return 0 ;
